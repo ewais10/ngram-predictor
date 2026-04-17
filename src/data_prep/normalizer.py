3419a3c67c64cbe3e8095ec28c1c51373add03ec
Muhammad Ewais <muhammad.ewais@analog.com>
2026-04-17 14:01:40 +0200

    - developed normalize method. - set other methods to static.

--- src/data_prep/normalizer.py
+++ src/data_prep/normalizer.py
@@ -6,12 +6,17 @@ import re
 class Normalizer:
     """The Normalizer class handles loading, cleaning, tokenizing, and saving the corpus."""
 
-    def normalize(self, text: str) -> str:
-        """Normalize text to lower-case and strip whitespace."""
+    def normalize(self, text: str, remPunc: bool = True) -> str:
+        """Normalize text to lower-case, remove punctuation (optionally), remove numbers and extra whitespaces."""
         if text is None:
             return ""
+        text = Normalizer.lowercase(text)
+        if remPunc: text = Normalizer.remove_punctuation(text)
+        text = Normalizer.remove_numbers(text)
+        text = Normalizer.remove_whitespace(text)        
         return text.strip().lower()
 
+    @staticmethod
     def load(folder: str | Path,  encoding: str = "utf-8") -> Dict[str, str]:
         """Load all .txt files from a folder in a dictionary."""
         p = Path(folder)
@@ -26,6 +31,7 @@ class Normalizer:
                     print(f"Warning: failed to read {f}: {e}")
         return texts
     
+    @staticmethod
     def strip_gutenberg(text: str):
         """Strip Gutenberg header and footer from text."""
         reTextStart = re.search(r"\*\*\*\ START OF THE PROJECT GUTENBERG EBOOK.*\*\*\*", text)
@@ -38,18 +44,22 @@ class Normalizer:
         text = text[gbStartIndex:gbEndIndex]
         return text;
 
+    @staticmethod
     def lowercase(text: str):
         """Convert text to lowercase."""
         return text.lower();
 
+    @staticmethod
     def remove_punctuation(text: str):
         """Remove punctuation from text."""
         return re.sub(r'[^\w\s]', '', text)
 
+    @staticmethod
     def remove_numbers(text: str):
         """Remove numbers from text."""
         return re.sub(r'\d', '', text)
 
+    @staticmethod
     def remove_whitespace(text: str):
         """Remove blank lines and extra spaces from text."""
         text = re.sub(r'\n\n+', '\n', text); # Remove blank lines
@@ -57,6 +67,11 @@ class Normalizer:
         text = re.sub(r'\n\s', '\n', text); # Remove white spaces at the beginning of lines
         return text
 
+    @staticmethod
+    def sentence_tokenize(text: str):
+        """Split text into sentences based on punctuations"""
+        return re.split(r'([,.!?;])', text)
+    
 if __name__ == "__main__":
     # Example usage
     dataN = Normalizer.load("data/raw/train")
@@ -64,15 +79,23 @@ if __name__ == "__main__":
     # print(dataN.keys())
     y=(dataN['data\\raw\\train\\108.txt'])
     newText = Normalizer.strip_gutenberg(y)
-    newText = Normalizer.lowercase(newText)
-    newText = Normalizer.remove_punctuation(newText)
-    newText = Normalizer.remove_numbers(newText)
-    newText = Normalizer.remove_whitespace(newText)
+    # newText = Normalizer.lowercase(newText)
+    # newText = Normalizer.remove_punctuation(newText)
+    # newText = Normalizer.remove_numbers(newText)
+    # newText = Normalizer.remove_whitespace(newText)
+    # newText = Normalizer.remove_whitespace(newText)
+    # newText = Normalizer.sentence_tokenize(newText)
 
+    normalizer = Normalizer()
+    newText = normalizer.normalize(newText, remPunc=False)
     # print in out file
     x = "hello"
+    # print(newText)
     with open("demofile.txt", "w", encoding="utf-8") as f:
-        f.write(str(newText))
+        f.write(newText)
+    # with open("demofile.txt", "w", encoding="utf-8") as f:
+    #     for item in newText:
+    #         f.write(item)
     # print(newText)
     # print(Normalizer.__doc__)
     # # help(Normalizer.load)
